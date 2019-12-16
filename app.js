e44bd54267fe9c1df4277aeb2f872c1a4575fb8c
Sabyasachi Ghosh <saghosh@dresourcesgroup.com>
2019-12-16 14:55:14 +0530

    Authentication

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -20,7 +21,7 @@ app.use("/images", express.static(path.join("images")));
 app.use((req,res,next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers",
-    "Origin, X-Requestd-With, Content-Type, Accept");
+    "Origin, X-Requestd-With, Content-Type, Accept, Authorization");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE, OPTIONS"
@@ -29,5 +30,5 @@ app.use((req,res,next) => {
 });
 
 app.use('/api/posts', postRoutes);
-
+app.use('/api/user', userRoutes);
 module.exports = app;
--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const Post = require('../models/post');
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 
 const MIME_TYPE_MAP = {
   'image/png': 'png',
@@ -26,13 +27,16 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post('', multer({storage: storage}).single("image"), (req,res,next) => {
-  const url = req.protocol + '://' + req.get("host");
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + "/images/" + req.file.filename
-  });
+router.post('',
+  checkAuth,
+  multer({storage: storage}).single("image"),
+  (req,res,next) => {
+    const url = req.protocol + '://' + req.get("host");
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: url + "/images/" + req.file.filename
+    });
   post.save().then(result => {
     res.status(201).json({
       message: 'Post Added successfully',
@@ -68,7 +72,7 @@ router.get('', (req, res, next) => {
     });
 });
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id',  (req, res, next) => {
     Post.findById(req.params.id)
       .then(post => {
         if(post){
@@ -79,28 +83,31 @@ router.get('/:id', (req, res, next) => {
       });
 });
 
-router.put('/:id', multer({storage: storage}).single("image"), (req, res, next) => {
-  let imagepath = req.body.imagePath;
-  if(req.file) {
-    const url = req.protocol + '://' + req.get("host");
-    imagepath = url + "/images/" + req.file.filename;
-  }
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: imagepath
-  });
-  console.log(post);
-  Post.updateOne({_id: req.params.id}, post )
-    .then(documents => {
-      res.status(200).json({
-        message: 'Posts Updated successfully!'
-      });
+router.put('/:id',
+  checkAuth,
+  multer({storage: storage}).single("image"),
+  (req, res, next) => {
+    let imagepath = req.body.imagePath;
+    if(req.file) {
+      const url = req.protocol + '://' + req.get("host");
+      imagepath = url + "/images/" + req.file.filename;
+    }
+    const post = new Post({
+      _id: req.body.id,
+      title: req.body.title,
+      content: req.body.content,
+      imagePath: imagepath
     });
+    console.log(post);
+    Post.updateOne({_id: req.params.id}, post )
+      .then(documents => {
+        res.status(200).json({
+          message: 'Posts Updated successfully!'
+        });
+      });
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAuth, (req, res, next) => {
   console.log(req.params.id);
   Post.deleteOne({_id: req.params.id}).then(result => {
     console.log(result);
